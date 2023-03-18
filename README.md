13a083de855c97370d62c9a607265a38a1203da3
Tiago S. Fraga <thyaggus@gmail.com>
2023-03-18 09:49:06 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Download
+
+A simple java project to download a large file using Servlet.
+The file is created on the fly in memory and it deliveries to client (browser) as multipart download. 
+In this way, it is avoid a huge uses of memory of JVM.
+
+The goals to download are:
+* PDF
+* CSV
+* TXT
+* OFX
+* XLSX
+* BBT
+
+For while, only CSV file was implemented.
---
+++ src/main/java/br/com/quasar/thyaggus/download/multiplart/servlet/MultiPartServlet.java
@@ -0,0 +1,67 @@
+//https://javadigest.wordpress.com/2012/02/13/downloading-multiple-files-using-multipart-response/
+
+package br.com.quasar.thyaggus.download.multiplart.servlet;
+
+import br.com.quasar.thyaggus.download.multiplart.service.MultipartService;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//https://javadigest.wordpress.com/2012/02/13/downloading-multiple-files-using-multipart-response/
+
+
+public class MultiPartServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    private static final int BUFFER_SIZE = -1;
+
+    private MultipartService service;
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("==========================[ INICIANDO DOWNLOAD ]==========================");
+
+        //https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Disposition
+
+
+        String looping = request.getParameter("multiplo");
+        long multiplo = 5;
+        if (looping != null && !looping.equals("")) {
+            multiplo = Integer.valueOf(looping);
+        }
+
+        String dispararErro = request.getParameter("erro");
+        if (dispararErro != null && !dispararErro.equals("")) {
+            throw new ServletException("Exceção forçada para tratamento de erro vindo do servlet!!");
+        }
+
+
+        // Set the response type and specify the boundary string
+        //response.setContentType("multipart/x-mixed-replace;boundary=???");
+        response.setContentType("application/octet-stream");
+        response.setHeader("Content-Disposition", "attachment; filename='csv.csv'");
+
+
+        response.setContentLength(BUFFER_SIZE);
+
+        ServletOutputStream out = response.getOutputStream();
+
+        service = new MultipartService();
+
+        for (int i = 0; i < multiplo; i++) {
+            byte[] stream = new byte[0];
+            try {
+                stream = service.gerarCSV(i);
+                out.write(stream, 0, stream.length);
+                out.flush();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        out.close();
+        System.out.println("==========================[ DOWNLOAD FINALIZADO ]=========================");
+    }
+}
